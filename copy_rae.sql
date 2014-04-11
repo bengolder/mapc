8d3bfb923ffd9a039d1b32c01dbbbb6336856c2b
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-04-11 11:00:12 -0400

    first gh-pages commit

--- copy_rae.sql
+++
@@ -1,6 +0,0 @@
-copy rae_public 
-    from '/Users/bgolder/projects/mapc/data/MassVehicleCensusData_20130310/rae_public_noheader.csv'
-    delimiter ','
-    csv
-;
-
--- export_grid_shp.sh
+++
@@ -1,6 +0,0 @@
-ogr2ogr \
-    ./data/sample_points.shp \
-    PG:"host=localhost dbname=mapc" \
-    -sql "select * from sample_points"
-
-

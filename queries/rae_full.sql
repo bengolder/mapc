853ea8b0c8dbbd382b58e74a3518f10b36deb1a2
Benjamin Golder <benjamin.j.golder@gmail.com>
2014-05-06 18:08:01 -0400

    Update rae_full.sql

--- queries/rae_full.sql
+++ queries/rae_full.sql
@@ -7,8 +7,18 @@
 -- 2010
 
 
--- create a table for records that fall within the desired date range
--- grab only passenger vehicles
+
+-- create a subtable from rae that only includes the records we are interested in.
+drop table if exists rae_2010;
+create table rae_2010 as (
+	select * from rae
+	where
+		(date '2010-01-01', date '2010-12-31') overlaps (start_date, end_date) and
+		owner_type = 1 and
+		mcycle is null
+);
+
+
 -- truncate the start and end dates to the year of 2010, and get the number of
 -- days that they overlap with 2010
 drop table if exists rae_days;
@@ -22,13 +32,23 @@ create table rae_days as (
         least(date '2010-12-31', end_date) - 
             greatest(date '2010-01-01', start_date
             ) as days_2010
-    from rae_public
-    where
-        -- only get records that overlap 2010
-        (date '2010-01-01', date '2010-12-31') overlaps (start_date, end_date) and
-        -- only use passenger vehicle
-        owner_type = 1
+    from rae_2010
 );
+
+-- using lookup tables to set mpg for manually fixed make and models.
+update rae_2010
+	set mpg2008 = b.mpg2008, mpg_adj = b.mpg2008
+from mpg_update as b
+where 
+	rae_2010.model_year = b.model_year and 
+	rae_2010.make = b.make and 
+	rae_2010.model = b.model;
+
+-- set the miles per day as null if they have none 
+-- (this accounts for those with invalid mileage estimates)
+update rae_2010
+	set mi_per_day = null
+where mi_per_day = 0;
 
 drop table if exists rae_nonnull;
 create table rae_nonnull as (
@@ -36,27 +56,36 @@ create table rae_nonnull as (
         r.record_id,
         r.vin_id,
         d.days_2010 as days_2010,
-
+        
         -- if there is no mpg value, then we will use the average mpg per mile
         -- driven
         ( case when r.mpg_adj is null then
             -- get mean adjusted miles per gallon for average mile driven
-            sum(r.mpg_adj * d.days_2010 * r.mi_per_day) / 
-            sum(d.days_2010 * r.mi_per_day)
+            ( select 
+            	sum(r.mpg_adj * d.days_2010 * r.mi_per_day) / 
+            	sum(d.days_2010 * r.mi_per_day)
+              from rae_days as d
+        		left join rae_2010 as r
+        		on d.record_id = r.record_id )
+
         else r.mpg_adj end ) as mpg_adj,
 
         -- if there is no valid mileage estimate, we will use the average
         -- number of miles driven per record in 2010
-        ( case when insp_match is null then
+        ( case when insp_match is false then
+        
             -- get the fleet mean miles traveled in 2010
-            sum( d.days_2010 * r.mi_per_day ) /
-            count(*)
+            ( select
+            	avg( r.mi_per_day )
+              from rae_days as d
+        		left join rae_2010 as r
+        		on d.record_id = r.record_id )
+            
         else r.mi_per_day end ) as miles_per_day,
-
+        
         r.veh_zip as zip
-
     from rae_days as d
-        left join rae_public as r
+        left join rae_2010 as r
         on d.record_id = r.record_id
 );
 
@@ -71,7 +100,3 @@ create table record_stats as (
         miles_per_day * days_2010 as miles_2010
     from rae_nonnull
 );
-
-
-
-
